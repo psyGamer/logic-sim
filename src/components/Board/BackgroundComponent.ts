e2319bd45fb78239fe249bb199887761edd394e1
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-12-04 18:34:12 +0100

    Improved visuals of the `BackgroundComponent`

--- src/components/Board/BackgroundComponent.ts
+++ src/components/Board/BackgroundComponent.ts
@@ -17,13 +17,18 @@ class BackgroundComponent extends CanvasComponent {
 	}
 
 	public draw(context: CanvasRenderingContext2D, zoom: number): void {
-		context.fillStyle = '#000';
-
 		const cameraWidth = this.camera.width / zoom;
 		const cameraHeight = this.camera.height / zoom;
 
+		context.fillStyle = '#121212';
+		context.fillRect(0, 0, cameraWidth, cameraHeight);
+
+		if (zoom < 0.4 - 0.00001) return;
+
 		const verticalPinCount = Math.ceil(cameraWidth / (this.tileSize + this.tileSpacing));
 		const horizontalPinCount = Math.ceil(cameraHeight / (this.tileSize + this.tileSpacing));
+
+		context.beginPath();
 
 		for (let x = 0; x < cameraWidth; x += this.tileSize + this.tileSpacing) {
 			for (let y = 0; y < cameraHeight; y += this.tileSize + this.tileSpacing) {
@@ -40,12 +45,14 @@ class BackgroundComponent extends CanvasComponent {
 				if (actualY + this.tileSize < 0) actualY += horizontalPinCount * (this.tileSize + this.tileSpacing);
 				if (actualY > cameraHeight) actualY -= horizontalPinCount * (this.tileSize + this.tileSpacing);
 
-				// Start drawing
-				context.beginPath();
-				context.arc(actualX, actualY, this.tileSize, 0, 2 * Math.PI);
-				context.fill();
+				context.rect(Math.floor(actualX), Math.floor(actualY), this.tileSize, this.tileSize);
 			}
 		}
+
+		// Fade out based on zoom
+		context.fillStyle = `rgba(44, 44, 44, ${(zoom - 0.4) / 5.2 + 0.5})`;
+		console.log(zoom, context.fillStyle);
+		context.fill();
 	}
 
 	private isVisible(xPos: number, yPos: number): boolean {
