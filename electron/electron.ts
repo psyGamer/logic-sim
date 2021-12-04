94a2c3288c3b9c17cefaa4ccad0c6629699ea32b
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-12-04 18:32:35 +0100

    Added electron support

---
+++ electron/electron.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+let mainWindow: Electron.BrowserWindow | null;
+
+function createWindow(): void {
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+		},
+	});
+
+	mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
